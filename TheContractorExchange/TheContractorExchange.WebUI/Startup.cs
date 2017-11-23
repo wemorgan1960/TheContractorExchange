2caf19641bb5c22286289b290383ed26e4b173cd
William E Morgan <bmorgan@telusplanet.net>
2017-11-23 10:58:54 -0700

    initial

---
+++ TheContractorExchange/TheContractorExchange.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheContractorExchange.WebUI.Startup))]
+namespace TheContractorExchange.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
