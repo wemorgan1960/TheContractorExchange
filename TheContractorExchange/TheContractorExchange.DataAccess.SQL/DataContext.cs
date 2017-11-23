66df4eb5905a723bfd41553208d482832323f433
William E Morgan <bmorgan@telusplanet.net>
2017-11-23 15:33:47 -0700

    Entity

---
+++ TheContractorExchange/TheContractorExchange.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheContractorExchange.Core.Models;
+
+namespace TheContractorExchange.DataAccess.SQL
+{
+    public class DataContext:DbContext
+    {
+        public DataContext()  : base("DefaultConnection") { }
+
+            public DbSet<Contractor> Contractors { get; set; }
+
+    }
+}
---
+++ TheContractorExchange/TheContractorExchange.DataAccess.SQL/Migrations/201711232233119_initial.cs
@@ -0,0 +1,33 @@
+namespace TheContractorExchange.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Contractors",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        City = c.String(),
+                        Province = c.String(),
+                        Country = c.String(),
+                        EmailAddress = c.String(),
+                        PhoneNumber = c.String(),
+                        CreateAT = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Contractors");
+        }
+    }
+}
